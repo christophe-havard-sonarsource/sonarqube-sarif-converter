3bd2a3e28aa88dbbfde29c88b89aa2468eaf440d
Christophe H <65390576+christophe-havard-sonarsource@users.noreply.github.com>
2023-02-28 15:20:14 +0100

    Update convert-issues-to-sarif.js

--- convert-issues-to-sarif.js
+++ convert-issues-to-sarif.js
@@ -1,21 +1,57 @@
-const issues = require('./issues.json');
+const jsonFile = require('./issues.json');
 
-const sarifIssues = issues.issues.map(issue => ({
-  ruleId: issue.rule,
-  level: issue.severity.toUpperCase(),
-  message: issue.message,
-  locations: [
-    {
-      physicalLocation: {
-        artifactLocation: {
-          uri: issue.component
+const fs = require('fs');
+
+const issues = JSON.parse(jsonFile);
+
+const sarifIssues = issues.issues.map((issue) => {
+  const ruleId = issue.rule;
+  const level = issue.severity.toUpperCase();
+  const message = issue.message;
+  const line = issue.line || 1;
+  const column = issue.column || 1;
+  const startLine = line;
+  const startColumn = column;
+  const endLine = line;
+  const endColumn = column;
+
+  return {
+    ruleId: ruleId,
+    level: level,
+    message: message,
+    locations: [
+      {
+        physicalLocation: {
+          artifactLocation: {
+            uri: issue.component,
+          },
+          region: {
+            startLine: startLine,
+            startColumn: startColumn,
+            endLine: endLine,
+            endColumn: endColumn,
+          },
         },
-        region: {
-          startLine: issue.line,
-          startColumn: issue.textRange.startOffset,
-          endColumn: issue.textRange.endOffset
-        }
-      }
-    }
-  ]
-}));
+      },
+    ],
+  };
+});
+
+const sarifResult = {
+  $schema: 'https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json',
+  version: '2.1.0',
+  runs: [
+    {
+      tool: {
+        driver: {
+          name: 'SonarQube',
+          rules: [],
+        },
+      },
+      results: sarifIssues,
+    },
+  ],
+};
+
+fs.writeFileSync('issues.sarif', JSON.stringify(sarifResult, null, 2));
+
